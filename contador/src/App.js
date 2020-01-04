927f778248fe774537420a515c17987ec34874ae
Patrik <patrikmatos.tecinfo@gmail.com>
2020-01-04 17:11:21 -0300

    Atualização
    - Remoção de App.test.js
    - Remoçao de logo.svg
    - Remoçao de CSS que não estão sendo utilizados
    - Adicionando components
    - Adicionando props

--- contador/src/App.js
+++ contador/src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+const MostrarVoltas = (props) => {
+  return(
+    <p>
+      {props.totalVoltas}<br />
+      {props.text}
+    </p>
+  );
+}
+
+const TempoVoltas = (props) => {
+  return(
+    <p>
+      {props.tempoMedio}<br />
+      {props.text}
+    </p>
+  );
+}
+
+const Button = (props) => {
+  return(
+    <button>{props.text}</button>
+  );
+}
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <MostrarVoltas
+        totalVoltas='10'
+        text='Voltas'
+      />
+      <Button 
+        text='+'
+      />
+      <Button 
+        text='-'
+      />
+      <TempoVoltas
+        tempoMedio='01:30'
+        text='Tempo Médio Por Voltas'
+      />
+     <Button
+      text='Iniciar'
+     />Iniciar
+     <Button
+      text='Reiniciar'
+     />
     </div>
   );
 }
 
-export default App;
