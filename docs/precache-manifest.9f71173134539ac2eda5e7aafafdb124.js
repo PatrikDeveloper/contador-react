0487c6adff2fbc2d430c81e4f1097a2f12499499
Patrik Matos <patrikmatos.tecinfo@gmail.com>
2020-04-06 17:52:26 -0300

    Build for demo in github

---
+++ docs/precache-manifest.9f71173134539ac2eda5e7aafafdb124.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a74a07f5f5e31b7afaeb91bb36c4740",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b97c581325ca1dc62d41",
+    "url": "/static/css/main.15405199.chunk.css"
+  },
+  {
+    "revision": "c4435702541f56b3a28e",
+    "url": "/static/js/2.b630d6b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b630d6b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b97c581325ca1dc62d41",
+    "url": "/static/js/main.43fd9b29.chunk.js"
+  },
+  {
+    "revision": "0582cedcbe9179d39d97",
+    "url": "/static/js/runtime-main.0d96ceb2.js"
+  }
+]);
